f594fdf6de06605886d3ac0ffb58dee597115d8d
yiting <>
2026-02-01 20:17:13 +0800

    ci(root): msw setting

---
+++ apps/portfolio/src/app/MSWProvider.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+export const MSWProvider = ({ children }: { children: React.ReactNode }) => {
+  const [mswReady, setMswReady] = useState(false);
+
+  useEffect(() => {
+    const init = async () => {
+      if (
+        typeof window !== 'undefined' &&
+        process.env.NODE_ENV === 'development'
+      ) {
+        const { worker } = await import('./api/mocks/browser');
+        await worker.start({
+          onUnhandledRequest: 'bypass',
+        });
+        setMswReady(true);
+      }
+    };
+
+    if (!mswReady) {
+      init();
+    }
+  }, [mswReady]);
+
+  if (!mswReady && process.env.NODE_ENV === 'development') {
+    return null; // or a loading spinner
+  }
+
+  return <>{children}</>;
+};
